5c8672581b01014d6f3653ccd81ce77d118162d3
romulo-soares <bromulosoares@gmail.com>
2018-08-14 20:29:33 -0300

    Comentando mani() desnecessário

--- src/main/java/br/edu/ifpb/ifpbcurso/IfpbCursoApplication.java
+++ src/main/java/br/edu/ifpb/ifpbcurso/IfpbCursoApplication.java
@@ -14,40 +14,40 @@ public class IfpbCursoApplication {
 
     public static void main(String[] args) {
 
-        Unidade unidade = new Unidade();
-        unidade.setNome("teste");
-        unidade.setId(new Long(1));
-
-        Disciplina disciplina = new Disciplina();
-        disciplina.setAbreviacao("RHT");
-        disciplina.setAulasSemana(6);
-        disciplina.setCargaHoraria(5);
-        disciplina.setNome("Recursos Humanos no Trabalho");
-        disciplina.setPeriodo(6);
-        disciplina.setTipo(TipoDisciplina.OBRIGATORIA);
-        disciplina.setId(new Long(1));
-
-        Disciplina disciplina1 = new Disciplina();
-        disciplina.setAbreviacao("ES");
-        disciplina.setAulasSemana(6);
-        disciplina.setCargaHoraria(5);
-        disciplina.setNome("Estágio");
-        disciplina.setPeriodo(6);
-        disciplina.setTipo(TipoDisciplina.OPTATIVA);
-        disciplina1.setId(new Long(2));
-
-        Curso curso = new Curso();
-        curso.setAbreviacao("ADS");
-        curso.setGrandeAreaId("Informática");
-        curso.setNome("Análise e Desenvolvimento de Sistemas");
-        curso.setPagina("");
-        curso.setPeriodos(6);
-        curso.setUnidade(unidade);
-        curso.addDisciplina(disciplina);
-        curso.addDisciplina(disciplina1);
-        curso.setId(new Long(1));
-
-        System.out.printf("Estes é o CURSO: " + curso.toString());
+//        Unidade unidade = new Unidade();
+//        unidade.setNome("teste");
+//        unidade.setId(new Long(1));
+//
+//        Disciplina disciplina = new Disciplina();
+//        disciplina.setAbreviacao("RHT");
+//        disciplina.setAulasSemana(6);
+//        disciplina.setCargaHoraria(5);
+//        disciplina.setNome("Recursos Humanos no Trabalho");
+//        disciplina.setPeriodo(6);
+//        disciplina.setTipo(TipoDisciplina.OBRIGATORIA);
+//        disciplina.setId(new Long(1));
+//
+//        Disciplina disciplina1 = new Disciplina();
+//        disciplina.setAbreviacao("ES");
+//        disciplina.setAulasSemana(6);
+//        disciplina.setCargaHoraria(5);
+//        disciplina.setNome("Estágio");
+//        disciplina.setPeriodo(6);
+//        disciplina.setTipo(TipoDisciplina.OPTATIVA);
+//        disciplina1.setId(new Long(2));
+//
+//        Curso curso = new Curso();
+//        curso.setAbreviacao("ADS");
+//        curso.setGrandeAreaId("Informática");
+//        curso.setNome("Análise e Desenvolvimento de Sistemas");
+//        curso.setPagina("");
+//        curso.setPeriodos(6);
+//        curso.setUnidade(unidade);
+//        curso.addDisciplina(disciplina);
+//        curso.addDisciplina(disciplina1);
+//        curso.setId(new Long(1));
+//
+//        System.out.printf("Estes é o CURSO: " + curso.toString());
 
         SpringApplication.run(IfpbCursoApplication.class, args);
     }
