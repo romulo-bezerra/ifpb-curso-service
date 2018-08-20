f99ebc4aeaa9265a962160f59df5a7177ec521e3
romulo-soares <bromulosoares@gmail.com>
2018-08-20 16:18:02 -0300

    Delete de pasta csv's e dependencias do main inutilizadas

--- src/main/java/br/edu/ifpb/ifpbcurso/IfpbCursoApplication.java
+++ src/main/java/br/edu/ifpb/ifpbcurso/IfpbCursoApplication.java
@@ -1,24 +1,9 @@
 package br.edu.ifpb.ifpbcurso;
 
-import br.edu.ifpb.ifpbcurso.config.ApplicationProperties;
-import br.edu.ifpb.ifpbcurso.domain.Curso;
-import br.edu.ifpb.ifpbcurso.domain.Disciplina;
-import br.edu.ifpb.ifpbcurso.domain.Unidade;
-import br.edu.ifpb.ifpbcurso.domain.enums.TipoDisciplina;
-import br.edu.ifpb.ifpbcurso.repository.CursoRepository;
-import br.edu.ifpb.ifpbcurso.service.CursoService;
-import br.edu.ifpb.ifpbcurso.util.LerCSV;
-import br.edu.ifpb.ifpbcurso.util.RestoreByCSV;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @EnableAutoConfiguration
 @SpringBootApplication
